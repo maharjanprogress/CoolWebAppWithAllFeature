98490d347583109e0224a8f656cc1a4cce674ef3
maharjanprogress <progressmaharjan1@gmail.com>
2025-12-05 15:23:10 +0545

    Loan Type Created

---
+++ backend/src/main/java/com/progress/coolProject/Enums/LoanType.java
@@ -0,0 +1,30 @@
+package com.progress.coolProject.Enums;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+public enum LoanType {
+    BUSINESS_LOAN("BUSINESS LOAN"),
+    EDUCATION_LOAN("EDUCATION LOAN"),
+    GHAR_LOAN("GHAR LOAN"),
+    HEALTH_LOAN("HEALTH LOAN"),
+    JIBIKOPARJAN_LOAN("JIBIKOPARJAN LOAN"),
+    OTHER_LOAN("OTHERS LOAN"),
+    PROVIDENT_FUND_LOAN("PROVIDENT FUND LOAN");
+
+    private final String loanTypeName;
+
+    LoanType(String loanTypeName) {
+        this.loanTypeName = loanTypeName;
+    }
+
+    public String getLoanTypeName() {
+        return loanTypeName;
+    }
+
+    public static Optional<LoanType> findByLoanTypeName(String loanTypeName) {
+        return Arrays.stream(values())
+                .filter(tb -> tb.getLoanTypeName().equalsIgnoreCase(loanTypeName.trim()))
+                .findFirst();
+    }
+}
